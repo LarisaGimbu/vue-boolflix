d55bfcd5b61c400cfead633fd25fd82c37f6840b
LarisaGimbu <gimbularisa@gmail.com>
2021-12-01 13:28:37 +0100

    loader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header @sendSearch="foundFilm"/>
+    <Loader v-if="this.loading"/>
     <Main :films="films"/>
   </div>
 </template>
@@ -8,28 +9,34 @@
 <script>
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
+import Loader from './components/Loader.vue'
 import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Main
+    Loader,
+    Main,
   },
   data(){
     return{
       films: [],
       apiURL: 'https://api.themoviedb.org/3/search/movie?',
       apiKey: '9e3e0024fc20b93902720482485f2a3e',
+      loading: false
     }
   },
   methods:{
     foundFilm(titolo){
-       axios 
+      this.loading = true;
+
+      axios 
         .get(`${this.apiURL}api_key=${this.apiKey}&query=${titolo}&language=it-IT`)
         .then((response) =>{
           this.films = response.data.results;
-
+          console.log(response);
+          this.loading = false;
           
         })
         .catch((error) =>{
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -23,7 +23,7 @@ export default {
     }
   },
   props:{
-    films: Array
+    films: Array,
   },
   
 }
