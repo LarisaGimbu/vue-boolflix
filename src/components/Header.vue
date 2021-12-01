4cd151602fc219c0bc697922e02fba4bee57f480
LarisaGimbu <gimbularisa@gmail.com>
2021-12-01 12:05:35 +0100

    main

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="d-flex justify-content-between">
+    <div class="logo ps-2">BOOLFLIX</div>
+    <div class="input-group mb-3 search-film">
+      <input type="text" class="form-control" placeholder="Inserisci il titolo del film" aria-describedby="button-addon2">
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/vars.scss';
+
+header{
+  .logo{
+    color: $logo-color;
+    font-size: 1.5rem;
+  }
+  .search-film{
+    width: 28%;
+    input{
+      width: 250px !important;
+      flex: none !important;
+    }
+  } 
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,52 @@
+<template>
+  <main class="container pt-5 d-flex justify-content-center flex-wrap">
+    <div
+    class="box pe-3" 
+    v-for="(film, index) in films"
+    :key="index">
+      <p><strong>Titolo:</strong> {{film.title}}</p>
+      <p><strong>Titolo originale:</strong> {{film.original_title}}</p>
+      <p><strong>Lingua originale:</strong> {{film.original_language}}</p> 
+      <p><strong>Voto:</strong> {{film.vote_average}}</p> 
+    </div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Main',
+  data(){
+    return{
+      films: [],
+    }
+  },
+
+  methods:{
+    getApi(){
+      axios
+        .get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro')
+        .then((response) =>{
+          this.films = response.data.results;
+
+          
+        })
+        .catch((error) =>{
+          console.log(error);
+        })
+    }
+  },
+  mounted(){
+    this.getApi()
+  }
+}
+</script>
+
+<style lang="scss">
+main{
+  .box{
+    width: 200px;
+  }
+}
+</style>
